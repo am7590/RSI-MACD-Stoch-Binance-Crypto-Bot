d019bc89d3b217262821b0a14eb010ce0c0ce8cc
Alek Michelson <am7590@rit.edu>
2022-02-12 18:09:19 -0500

    Got binance data in a pandas DF

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RSI-MACD-Stoch-Binance-Crypto-Bot
 Originally from this tutorial: https://www.youtube.com/watch?v=X50-c54BWV8&t=282s
+
+## Strategy Buying Conditions:
+- Stochastic (%K and %D) must be between 20 and 80
+- MACD diff > 0
+- In the last n time steps the %K and %D lines have to cross below 20
+
+## Strategy Selling Conditions
+- Stop loss: X amount (99.5%?) of selling price
+- Take profit: 1.005 * buying price
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import binance
+from binance import Client
+import pandas as pd
+import ta
+import numpy as np
+import time
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def connect_to_client():
+    # API Key, API Private
+    client = Client("CDbwg0t1i5U8PmfMwhhwPr9EMjj6v2rRpQ88P50mLHEZs5CNNfSCCZJ3KPKfW3Hd",
+                    "xFKfmX4IqtGh790vBzmkDb7xwm790EhcBsZMjWa5pllJroJm9e57xH8bBTU8gxHg")
+    return client
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def get_minute_data(symbol, interval, lookback):
+    client = connect_to_client()
+    frame = pd.DataFrame(client.get_historical_klines(symbol, interval, lookback + ' min ago UTC'))
+    frame = frame.iloc[:,:6]
+    frame.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
+    frame = frame.set_index('Time')
+    frame.index = pd.to_datetime(frame.index, unit='ms')
+    frame = frame.astype(float)
+
+    return frame
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    df = get_minute_data("ADAUSDT", "1m", "100")
+    print(df)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
